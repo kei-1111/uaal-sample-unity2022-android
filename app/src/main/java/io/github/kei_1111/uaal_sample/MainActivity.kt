5e2afa8a881bbff1590f98eb6e3be001a79e3f15
kei-1111 <kei_02_1111@icloud.com>
2025-10-11 00:17:49 +0900

    chore: レイアウト作成

--- app/src/main/java/io/github/kei_1111/uaal_sample/MainActivity.kt
+++ app/src/main/java/io/github/kei_1111/uaal_sample/MainActivity.kt
@@ -1,10 +1,20 @@
 package io.github.kei_1111.uaal_sample
 
+import android.R.attr.onClick
+import android.R.attr.text
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.navigationBarsPadding
+import androidx.compose.foundation.layout.safeDrawingPadding
+import androidx.compose.foundation.layout.safeGesturesPadding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.viewinterop.AndroidView
 import com.unity3d.player.UnityPlayer
@@ -22,10 +32,28 @@ class MainActivity : ComponentActivity() {
 
         setContent {
             UaalsampleTheme {
-                AndroidView(
-                    factory = { unityPlayer!! },
+                Box(
                     modifier = Modifier.fillMaxSize()
-                )
+                ) {
+                    AndroidView(
+                        factory = { unityPlayer!! },
+                        modifier = Modifier.fillMaxSize()
+                    )
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .safeGesturesPadding()
+                    ) {
+                        Button(
+                            onClick = {},
+                            modifier = Modifier.align(Alignment.BottomCenter)
+                        ) {
+                            Text(
+                                text = "Send Message to Unity"
+                            )
+                        }
+                    }
+                }
             }
         }
     }
