36c17c2c647dbebc1ded213d96719878fa563450
kei-1111 <kei_02_1111@icloud.com>
2025-10-11 00:14:06 +0900

    feat: Unityが動くように実装

--- app/src/main/java/io/github/kei_1111/uaal_sample/MainActivity.kt
+++ app/src/main/java/io/github/kei_1111/uaal_sample/MainActivity.kt
@@ -5,43 +5,43 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.viewinterop.AndroidView
+import com.unity3d.player.UnityPlayer
 import io.github.kei_1111.uaal_sample.ui.theme.UaalsampleTheme
 
 class MainActivity : ComponentActivity() {
+
+    private var unityPlayer: UnityPlayer? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
+
+        unityPlayer = UnityPlayer(this)
+
         setContent {
             UaalsampleTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
+                AndroidView(
+                    factory = { unityPlayer!! },
+                    modifier = Modifier.fillMaxSize()
+                )
             }
         }
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+        super.onWindowFocusChanged(hasFocus)
+        unityPlayer?.windowFocusChanged(hasFocus)
+    }
 
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    UaalsampleTheme {
-        Greeting("Android")
+    override fun onResume() {
+        super.onResume()
+        unityPlayer?.resume()
+    }
+
+    override fun onPause() {
+        super.onPause()
+        unityPlayer?.pause()
     }
 }
